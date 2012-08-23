6b80b54f091bc40e91195bdfabdb8fe08e4b973b
Eric Rochester <err8n@virginia.edu>
2012-08-23 09:14:02 -0400

    Fix date formatting and pulling articles for index page.

--- site.hs
+++ site.hs
@@ -13,22 +13,22 @@ main = hakyll $ do
     match "index.html" $ route idRoute
     create "index.html" $ constA mempty
         >>> arr (setField "title" "Eric Rochester &loz; Home")
-        >>> requireAllA "articles/*.md" (arr id *** arr ( L.take 3
-                                                        . L.reverse
-                                                        . L.sortBy comparePagesByDate
-                                                        )
-                                        >>> addPostList "articles"
-                                        )
+        >>> requireAllA "articles/*" (arr id *** arr ( L.take 3
+                                                     . L.reverse
+                                                     . L.sortBy comparePagesByDate
+                                                     )
+                                     >>> addPostList "articles"
+                                     )
 
         -- requireAllA "notes/*.md"
-        -- >>> applyTemplateCompiler "templates/index.html"
+        >>> applyTemplateCompiler "templates/index.html"
         >>> applyTemplateCompiler "templates/default.html"
         >>> relativizeUrlsCompiler
 
     match "articles/*" $ do
         route   $ setExtension "html"
         compile $   pageCompiler
-                >>> arr (renderDateField "published" "%D %e, %Y, at %H:%M" "unknown")
+                >>> arr (renderDateField "published" "%B %e, %Y, at %H:%M" "unknown")
                 >>> applyTemplateCompiler "templates/article.html"
                 >>> applyTemplateCompiler "templates/default.html"
                 >>> relativizeUrlsCompiler
@@ -66,10 +66,13 @@ main = hakyll $ do
 
 addPostList :: String -> Compiler (Page String, [Page String]) (Page String)
 addPostList field = setFieldA field $
-        require "templates/articleitem.html" (\p t -> map (applyTemplate t) p)
+        require "templates/articleitem.html" renderPost
     >>> arr mconcat
     >>> applyTemplateCompiler "templates/articlediv.html"
     >>> arr pageBody
+    where
+        renderPost :: [Page String] -> Template -> [Page String]
+        renderPost p t = map (applyTemplate t) p
 
 compassCompiler :: Compiler Resource String
 compassCompiler =   getResourceString
