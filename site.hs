864ae009755e33a7b8323ec73f66222bc8367c00
Eric Rochester <err8n@virginia.edu>
2012-08-23 08:54:01 -0400

    Add rules for articles and the index page.

--- site.hs
+++ site.hs
@@ -2,11 +2,36 @@
 
 
 import Control.Arrow
+import Data.Monoid
 import Hakyll
+import qualified Data.List as L
 
 
 main :: IO ()
 main = hakyll $ do
+
+    match "index.html" $ route idRoute
+    create "index.html" $ constA mempty
+        >>> arr (setField "title" "Eric Rochester &loz; Home")
+        >>> requireAllA "articles/*.md" (arr id *** arr ( L.take 3
+                                                        . L.reverse
+                                                        . L.sortBy comparePagesByDate
+                                                        )
+                                        >>> addPostList "articles"
+                                        )
+
+        -- requireAllA "notes/*.md"
+        -- >>> applyTemplateCompiler "templates/index.html"
+        >>> applyTemplateCompiler "templates/default.html"
+        >>> relativizeUrlsCompiler
+
+    match "articles/*" $ do
+        route   $ setExtension "html"
+        compile $   pageCompiler
+                >>> arr (renderDateField "published" "%D %e, %Y, at %H:%M" "unknown")
+                >>> applyTemplateCompiler "templates/article.html"
+                >>> applyTemplateCompiler "templates/default.html"
+                >>> relativizeUrlsCompiler
 
     match "*.md" $ do
         route   $ setExtension "html"
@@ -38,6 +63,13 @@ main = hakyll $ do
     match "js/*" $ do
         route   idRoute
         compile copyFileCompiler
+
+addPostList :: String -> Compiler (Page String, [Page String]) (Page String)
+addPostList field = setFieldA field $
+        require "templates/articleitem.html" (\p t -> map (applyTemplate t) p)
+    >>> arr mconcat
+    >>> applyTemplateCompiler "templates/articlediv.html"
+    >>> arr pageBody
 
 compassCompiler :: Compiler Resource String
 compassCompiler =   getResourceString
