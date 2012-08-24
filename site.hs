3c669f3987891193a76c3d5ca305257ada82b025
Eric Rochester <err8n@virginia.edu>
2012-08-24 06:30:50 -0400

    Fix date sorting and printing and made addPostList more general.

--- site.hs
+++ site.hs
@@ -7,17 +7,23 @@ import Hakyll
 import qualified Data.List as L
 
 
+dateFormat :: String
+dateFormat = "%B %e, %Y %l:%M %p"
+
+
 main :: IO ()
 main = hakyll $ do
 
-    match "index.html" $ route idRoute
+    match  "index.html" $ route idRoute
     create "index.html" $ constA mempty
         >>> arr (setField "title" "Eric Rochester &loz; Home")
-        >>> requireAllA "articles/*" (arr id *** arr ( L.take 3
-                                                     . L.reverse
-                                                     . L.sortBy comparePagesByDate
-                                                     )
+        >>> requireAllA "articles/*" (   arr id *** arr ( L.take 3
+                                                        . L.reverse
+                                                        . L.sortBy comparePagesByDate
+                                                        )
                                      >>> addPostList "articles"
+                                                     "templates/articleitem.html"
+                                                     "templates/articlediv.html"
                                      )
 
         -- requireAllA "notes/*.md"
@@ -28,7 +34,7 @@ main = hakyll $ do
     match "articles/*" $ do
         route   $ setExtension "html"
         compile $   pageCompiler
-                >>> arr (renderDateField "published" "%B %e, %Y, at %H:%M" "unknown")
+                >>> arr (renderDateField "published" dateFormat "unknown")
                 >>> applyTemplateCompiler "templates/article.html"
                 >>> applyTemplateCompiler "templates/default.html"
                 >>> relativizeUrlsCompiler
@@ -64,11 +70,12 @@ main = hakyll $ do
         route   idRoute
         compile copyFileCompiler
 
-addPostList :: String -> Compiler (Page String, [Page String]) (Page String)
-addPostList field = setFieldA field $
-        require "templates/articleitem.html" renderPost
+addPostList :: String -> Identifier Template -> Identifier Template
+            -> Compiler (Page String, [Page String]) (Page String)
+addPostList field itemTemplate wrapperTemplate = setFieldA field $
+        require itemTemplate renderPost
     >>> arr mconcat
-    >>> applyTemplateCompiler "templates/articlediv.html"
+    >>> applyTemplateCompiler wrapperTemplate
     >>> arr pageBody
     where
         renderPost :: [Page String] -> Template -> [Page String]
@@ -80,5 +87,4 @@ compassCompiler =   getResourceString
                                       , "--load-path", "./sass"
                                       ]
                 >>> arr compressCss
-
 
