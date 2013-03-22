0855fb5c2a7d5e2eca750cee269f46212e33a399
Eric Rochester <erochest@virginia.edu>
2013-03-21 22:23:43 -0400

    Split the site up when deploying.

--- deploy.hs
+++ deploy.hs
@@ -1,31 +1,31 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE RecordWildCards   #-}
+
+
+-- | Passing in the option `--bail` will cause it to skip commiting and pushing
+-- the changes.
+
 
 module Main where
 
 
 import           ClassyPrelude
 import           Data.Time
+import qualified Data.List as L
 import qualified Data.Set as S
 import qualified Data.Text.Lazy as TL
-import           Shelly
+import           Shelly hiding ((</>))
+import           Sites (site, SiteInfo(..), RootSite(..))
 import           System.Locale
 
 default (TL.Text)
 
--- TODO: At this stage, break out the subprojects.
+binDir :: FilePath
+binDir = "./dist/build/site"
 
-toSave :: S.Set FilePath
-toSave = S.fromList [ "_deploy/.git"
-                    , "_deploy/CNAME"
-                    ]
-
-
-ghc_ :: [TL.Text] -> Sh ()
-ghc_ = command_ "ghc" [ "--make"
-                      , "-package-conf"
-                      , "./cabal-dev/packages-7.4.2.conf"
-                      ]
+cabalDev_ :: TL.Text -> [TL.Text] -> Sh ()
+cabalDev_ = command1_ "cabal-dev" []
 
 git_ :: TL.Text -> [TL.Text] -> Sh ()
 git_ = command1_ "git" []
@@ -34,29 +34,49 @@ gitCommit :: TL.Text -> Sh ()
 gitCommit msg = git_ "commit" ["-m", msg]
 
 clearDeploy :: Sh ()
-clearDeploy = mapM_ rm_rf =<< filter (`S.notMember` toSave) <$> ls "_deploy"
+clearDeploy =
+        ls "_deploy/" >>=
+        fmap (filter ((".git" /=) . filename) . concat) . mapM ls >>=
+        mapM_ rm_rf
 
-copySite :: Sh ()
-copySite = mapM_ (`cp_r` "_deploy") =<< ls "_site"
+copySite :: FilePath -> Sh ()
+copySite dest = mapM_ (`cp_r` dest) =<< ls "_site"
 
-commitDeploy :: Sh ()
-commitDeploy = undefined
+deploySite :: TL.Text -> TL.Text -> FilePath -> Sh ()
+deploySite branch msg dir =
+        chdir dir $ do
+            git_ "checkout" ["--branch", branch]
+            git_ "add" ["--all"]
+            gitCommit msg
+            git_ "push" ["origin", "master"]
 
 
 main :: IO ()
 main = shelly $ verbosely $ do
-    now <- TL.pack . formatTime defaultTimeLocale rfc822DateFormat <$> liftIO getCurrentTime
-    ghc_ ["site.hs"]
-    run_ "./site" ["rebuild"]
-    clearDeploy
-    copySite
+    args <-  getArgs
+    now  <-  TL.pack . formatTime defaultTimeLocale rfc822DateFormat
+         <$> liftIO getCurrentTime
+    Root{..} <- liftIO site
+    let rootDeploy = ("_deploy" </>) $ siteTarget rootSite
 
-    -- TODO: Really need to have a command-line argument for bailing here. That
-    -- would make testing easier.
-    chdir "_deploy" $ do
-        git_ "add" ["--all"]
-        gitCommit $ "Deployed on " ++ now ++ "."
-        git_ "push" ["origin", "master"]
-    git_ "add" ["_deploy"]
-    gitCommit $ "Deployed on " ++ now ++ "."
+    cabalDev_ "clean"     []
+    cabalDev_ "configure" []
+    cabalDev_ "build"     []
+
+    run_ (binDir </> "site") ["rebuild"]
+    clearDeploy
+    copySite rootDeploy
+    forM_ subsites $ \site ->
+        let src  = "_deploy" </> siteTarget rootSite </> siteRoot site
+            dest = "_deploy" </> siteTarget site
+        in  ls src >>=
+            mapM_ (`mv` dest) >>
+            rm_rf src
+
+    unless ("--bail" `elem` args) $ do
+        let msg = "Deployed on " ++ now ++ "."
+        deploySite "master" msg rootDeploy
+        forM_ subsites (deploySite "gh-pages" msg . ("_deploy" </>) . siteTarget)
+        git_ "add" ["_deploy"]
+        gitCommit msg
 
