986658fa94f3c25e09b5820c761d85811eddf8dd
Eric Rochester <erochest@virginia.edu>
2013-04-03 09:40:06 -0400

    Parse metadata when reading a literate code file.

--- Sites/Literate.hs
+++ Sites/Literate.hs
@@ -3,16 +3,23 @@
 module Sites.Literate
     ( CommentSpec(..)
     , CommentSpecList
+    , MetadataList
     , illiterate
+    , illiterateHtml
     , markdown
     , clojure
+    , clojureComments
+    , addMetadataList
+    , metadataListContext
     ) where
 
 
+import           Control.Arrow ((&&&), first, second)
 import qualified Data.List as L
 import           Data.Maybe
 import           Data.Monoid
 import           Hakyll
+import           Sites.Base
 import           Text.Pandoc
 
 
@@ -21,6 +28,7 @@ data CommentSpec = CommentLine String
                  deriving (Show)
 
 type CommentSpecList = [CommentSpec]
+type MetadataList    = [(String, String)]
 
 -- | Idle thought: I wonder if breaking this into two levels of types would
 -- simplify or complicate the combination rules (`mappend`) below. My intuition
@@ -30,6 +38,7 @@ type CommentSpecList = [CommentSpec]
 -- this is still something to try later.
 data RunBlock = LCodeBlock ![String]
               | LTextBlock ![String]
+              | MDBlock    !MetadataList
               | BlockRun   ![RunBlock]
               deriving (Show)
 
@@ -38,35 +47,48 @@ instance Monoid RunBlock where
 
         mappend a (BlockRun []) = a
         mappend (BlockRun []) b = b
+        mappend (MDBlock a)    (MDBlock b)    = MDBlock $ a ++ b
         mappend (LCodeBlock a) (LCodeBlock b) = LCodeBlock $ a ++ b
         mappend (LTextBlock a) (LTextBlock b) = LTextBlock $ a ++ b
-        mappend a@(LCodeBlock _) b@(LTextBlock _) = BlockRun [a, b]
-        mappend a@(LTextBlock _) b@(LCodeBlock _) = BlockRun [a, b]
+        mappend (MDBlock a)    (BlockRun (MDBlock b:br))    = BlockRun $ MDBlock    (a ++ b) : br
         mappend (LCodeBlock a) (BlockRun (LCodeBlock b:br)) = BlockRun $ LCodeBlock (a ++ b) : br
         mappend (LTextBlock a) (BlockRun (LTextBlock b:br)) = BlockRun $ LTextBlock (a ++ b) : br
+        mappend a@(MDBlock _)    (BlockRun br) = BlockRun (a:br)
         mappend a@(LCodeBlock _) (BlockRun br) = BlockRun (a:br)
         mappend a@(LTextBlock _) (BlockRun br) = BlockRun (a:br)
+        mappend (BlockRun br) (MDBlock b) =
+            case last br of
+                MDBlock a -> BlockRun $ init br ++ [MDBlock (a ++ b)]
+                b         -> BlockRun $ br ++ [b]
         mappend (BlockRun br) (LCodeBlock b) =
             case last br of
-                LCodeBlock a     -> BlockRun $ init br ++ [LCodeBlock (a ++ b)]
-                t@(LTextBlock _) -> BlockRun $ br ++ [t]
+                LCodeBlock a -> BlockRun $ init br ++ [LCodeBlock (a ++ b)]
+                b            -> BlockRun $ br ++ [b]
         mappend (BlockRun br) (LTextBlock b) =
             case last br of
-                LTextBlock a     -> BlockRun $ init br ++ [LTextBlock (a ++ b)]
-                c@(LCodeBlock _) -> BlockRun $ br ++ [c]
+                LTextBlock a -> BlockRun $ init br ++ [LTextBlock (a ++ b)]
+                b            -> BlockRun $ br ++ [b]
         mappend (BlockRun a) b@(BlockRun _) = foldr mappend b a
+        mappend a b = BlockRun [a, b]
 
+
+pullMetadata :: RunBlock -> MetadataList
+pullMetadata (MDBlock md)   = md
+pullMetadata (LTextBlock _) = []
+pullMetadata (LCodeBlock _) = []
+pullMetadata (BlockRun xs)  = concatMap pullMetadata xs
 
 nl :: [String] -> [String]
 nl = map (++ "\n")
 
 renderBlock :: String -> RunBlock -> String
+renderBlock _         (MDBlock _)    = ""
 renderBlock _         (LTextBlock x) = concat $ nl x
 renderBlock codeClass (LCodeBlock x) = concat . nl $ (("```" ++ codeClass):x) ++ ["```"]
 renderBlock codeClass (BlockRun xs)  = L.intercalate "\n" $ map (renderBlock codeClass) xs
 
 isEmpty :: RunBlock -> Bool
-isEmpty (LCodeBlock []) = True
+isEmpty (LCodeBlock cs) | not (any (not . L.null) cs) = True
 isEmpty (LTextBlock []) = True
 isEmpty (BlockRun   []) = True
 isEmpty (BlockRun   br) = L.all isEmpty br
@@ -75,10 +97,8 @@ isEmpty _               = False
 removeEmpty :: [RunBlock] -> [RunBlock]
 removeEmpty = filter (not . isEmpty)
 
--- TODO: include code stylesheet.
-
-illiterate :: String -> CommentSpecList -> String -> String
-illiterate codeClass specList = renderBlock codeClass
+illiterate :: String -> CommentSpecList -> String -> (MetadataList, String)
+illiterate codeClass specList = (pullMetadata &&& renderBlock codeClass)
                               . foldr mappend mempty
                               . removeEmpty
                               . snd
@@ -86,6 +106,7 @@ illiterate codeClass specList = renderBlock codeClass
                               . lines
 
 data CommentState = Start
+                  | InMetadata
                   | CommentText String String
                   | ActiveText
 
@@ -103,9 +124,10 @@ illiterateLine :: CommentSpecList
                -> CommentState
                -> String
                -> (CommentState, RunBlock)
-illiterateLine _ Start ('#':'!':_) = (ActiveText, mempty)
+illiterateLine _ Start ('#':'!':_) = (Start, mempty)
 
 illiterateLine [] Start               line = (ActiveText,  LCodeBlock [line])
+illiterateLine [] InMetadata          line = (ActiveText,  MDBlock    [])
 illiterateLine [] ActiveText          line = (ActiveText,  LCodeBlock [line])
 illiterateLine [] c@(CommentText _ _) line = (c,           LTextBlock [line])
 
@@ -114,11 +136,17 @@ illiterateLine _ c@(CommentText end strip) line
     | otherwise               = (ActiveText, LTextBlock [stripStart strip line])
 
 illiterateLine (CommentLine cs:css) Start line
+    | line == cs ++ "---"    = (InMetadata, mempty)
     | cs `L.isPrefixOf` line = (ActiveText, LTextBlock [stripStart cs line])
     | otherwise              = illiterateLine css Start line
 illiterateLine (CommentBlock start end strip:css) Start line
     | start `L.isPrefixOf` line = (CommentText end strip, LTextBlock [stripStart start line])
     | otherwise                 = illiterateLine css Start line
+
+illiterateLine (CommentLine cs:css) InMetadata line
+    | line == cs ++ "---"    = (ActiveText, mempty)
+    | cs `L.isPrefixOf` line = (InMetadata, MDBlock [splitPair $ stripStart cs line])
+    | otherwise              = illiterateLine css InMetadata line
 
 illiterateLine (CommentLine cs:css) ActiveText line
     | cs `L.isPrefixOf` line = (ActiveText, LTextBlock [stripStart cs line])
@@ -127,11 +155,31 @@ illiterateLine (CommentBlock start end strip:css) ActiveText line
     | start `L.isPrefixOf` line = (CommentText end strip, LTextBlock [stripStart start line])
     | otherwise                 = illiterateLine css ActiveText line
 
-clojure :: Item String -> Item String
-clojure = markdown . fmap (illiterate "clojure" [ CommentLine "; "
-                                                , CommentLine ";"
-                                                ])
+splitPair :: String -> (String, String)
+splitPair = first trim . second trim . break (== ':')
 
-markdown :: Item String -> Item String
-markdown = writePandoc . fmap (readMarkdown defaultHakyllReaderOptions)
+illiterateHtml :: String -> CommentSpecList -> String -> (MetadataList, String)
+illiterateHtml codeClass specList = second markdown . illiterate codeClass specList
+
+clojure :: String -> (MetadataList, String)
+clojure = illiterateHtml "clojure" clojureComments
+
+clojureComments :: CommentSpecList
+clojureComments = [ CommentLine "; "
+                  , CommentLine ";"
+                  ]
+
+markdown :: String -> String
+markdown = debug
+         . writeHtmlString defaultHakyllWriterOptions
+         . readMarkdown defaultHakyllReaderOptions
+
+addMetadataList :: MetadataList -> Context String -> Context String
+addMetadataList = flip (foldr merge)
+    where
+        merge ("date", d) = mappend (dateContext d)
+        merge (a, b)      = mappend (constField a b)
+
+metadataListContext :: MetadataList -> Context String
+metadataListContext = (`addMetadataList` mempty)
 
