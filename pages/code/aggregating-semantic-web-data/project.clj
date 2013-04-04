cb10d0c11ffcfbdbff0eb76631d5c9919a7eb6dc
Eric Rochester <erochest@virginia.edu>
2013-04-04 14:33:01 -0400

    Draft "Aggregating Semantic Web Data."

---
+++ pages/code/aggregating-semantic-web-data/project.clj
@@ -0,0 +1,10 @@
+(defproject aggregating-semantic-web-data "0.1.0-SNAPSHOT"
+  :description "A recipe for aggregating semantic web data."
+  :url "http://www.ericrochester.com/pages/code/aggregating-semantic-web-data/"
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [incanter/incanter "1.4.1"]
+                 [edu.ucdenver.ccp/kr-sesame-core "1.4.5"]
+                 [org.clojure/tools.logging "0.2.4"]
+                 [org.slf4j/slf4j-simple "1.7.2"]]
+  :source-paths ["."])
+
