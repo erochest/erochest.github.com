497ddeb5db3859ff4ad7085e7b999a1e6c4a6ef2
Eric Rochester <erochest@virginia.edu>
2013-05-22 09:56:03 -0400

    Removed unpublished pages.

--- pages/code/a-simple-dataflow-system/project.clj
+++
@@ -1,6 +0,0 @@
-(defproject simple-dataflow "0.1.0-SNAPSHOT"
-  :description "A simple dataflow system."
-  :url "http://www.ericrochester.com/pages/code/a-simple-dataflow-system/"
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :source-paths ["."])
-
