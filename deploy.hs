2cdcee3440cfb4e43c8d8b593a9a79ef06148ded
Eric Rochester <erochest@virginia.edu>
2016-05-25 06:40:37 -0400

    Removed system-filepath.

--- deploy.hs
+++ deploy.hs
@@ -14,16 +14,18 @@
 module Main where
 
 
-import           ClassyPrelude             hiding (FilePath, concat, whenM,
-                                            (</>), (<>))
+import           ClassyPrelude       hiding (FilePath, concat, whenM, (</>),
+                                      (<>))
 import           Data.Data
-import qualified Data.Text                 as T
-import qualified Data.Text.Lazy            as TL
+import qualified Data.Text           as T
+import qualified Data.Text.Lazy      as TL
 import           Data.Time
-import           Filesystem.Path.CurrentOS
 import           Options.Applicative
-import           Shelly                    hiding (FilePath, (</>))
-import           Sites                     (RootSite (..), SiteInfo (..), site)
+import           Shelly              hiding (FilePath, (</>))
+import qualified Shelly              as S
+import           Sites               (RootSite (..), SiteInfo (..), site)
+import           System.FilePath
+import qualified System.FilePath     as FP
 
 default (TL.Text)
 
@@ -41,20 +43,27 @@ clearDeploy :: Sh ()
 clearDeploy =
         ls "_deploy/"
     >>= mapM ls
-    >>= mapM_ rm_rf . filter ((".git" /=) . filename) . fold
+    >>= mapM_ rm_rf
+    .   filter ((".git" /=) . FP.takeFileName . T.unpack . toTextIgnore)
+    .   fold
 
 copySite :: FilePath -> Sh ()
-copySite dest = mapM_ (`cp_r` dest) =<< ls "_site"
+copySite dest = mapM_ (`cp_r` dest') =<< ls "_site"
+    where
+        dest' = filePathString dest
 
 deploySite :: Text -> Text -> FilePath -> Sh ()
 deploySite branch msg dir =
-    chdir dir $ do
+    chdir (filePathString dir) $ do
         git_ "checkout" [branch]
         git_ "add" ["--all"]
         errExit False $ do
             gitCommit msg
             whenM ((== 0) <$> lastExitCode) $
                 git_ "push" []
+
+filePathString :: String -> S.FilePath
+filePathString = fromText . T.pack
 
 
 main :: IO ()
@@ -75,8 +84,8 @@ main = do
         clearDeploy
         copySite rootDeploy
         forM_ subsites $ \s ->
-            let src  = "_deploy" </> siteTarget rootSite </> siteRoot s
-                dest = "_deploy" </> siteTarget s
+            let src  = filePathString $ "_deploy" </> siteTarget rootSite </> siteRoot s
+                dest = filePathString $ "_deploy" </> siteTarget s
             in  ls src >>=
                 mapM_ (`mv` dest) >>
                 rm_rf src
--- src/Sites/Types.hs
+++ src/Sites/Types.hs
@@ -3,15 +3,15 @@ module Sites.Types where
 
 
 import qualified Data.Text                 as T
-import qualified Filesystem.Path.CurrentOS as FP
 import           Hakyll
 
 
 data SiteInfo = Site
-              { siteName   :: T.Text                -- ^ An identifier.
-              , siteTarget :: FP.FilePath           -- ^ Where to put the site (under `_deploy`).
-              , siteRoot   :: FP.FilePath           -- ^ The root of the site under `_site`.
-              , siteRules  :: Rules ()              -- ^ These are the Hakyll rules for building this site.
+              { siteName   :: T.Text     -- ^ An identifier.
+              , siteTarget :: FilePath   -- ^ Where to put the site (under `_deploy`).
+              , siteRoot   :: FilePath   -- ^ The root of the site under `_site`.
+              , siteRules  :: Rules ()   -- ^ These are the Hakyll rules for building
+                                         -- this site.
               }
 
 data RootSite = Root
