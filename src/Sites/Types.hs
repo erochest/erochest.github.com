607b58364134593016f542655625ba73ec2b2d48
Eric Rochester <erochest@virginia.edu>
2016-07-20 05:48:05 -0400

    Reformatting

--- src/Sites/Types.hs
+++ src/Sites/Types.hs
@@ -6,16 +6,18 @@ import qualified Data.Text                 as T
 import           Hakyll
 
 
-data SiteInfo = Site
-              { siteName   :: T.Text     -- ^ An identifier.
-              , siteTarget :: FilePath   -- ^ Where to put the site (under `_deploy`).
-              , siteRoot   :: FilePath   -- ^ The root of the site under `_site`.
-              , siteRules  :: Rules ()   -- ^ These are the Hakyll rules for building
-                                         -- this site.
-              }
+data SiteInfo
+    = Site
+    { siteName   :: T.Text     -- ^ An identifier.
+    , siteTarget :: FilePath   -- ^ Where to put the site (under `_deploy`).
+    , siteRoot   :: FilePath   -- ^ The root of the site under `_site`.
+    , siteRules  :: Rules ()   -- ^ These are the Hakyll rules for building
+                               -- this site.
+    }
 
-data RootSite = Root
-              { rootSite :: SiteInfo
-              , subsites :: [SiteInfo]
-              }
+data RootSite
+    = Root
+    { rootSite :: SiteInfo
+    , subsites :: [SiteInfo]
+    }
 
