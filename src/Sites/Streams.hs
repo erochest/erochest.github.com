2f42a88b410b6ac260ae225e00287c5a0783b7a1
Eric Rochester <erochest@virginia.edu>
2016-10-21 05:42:59 -0400

    Stub in streams.

---
+++ src/Sites/Streams.hs
@@ -0,0 +1,10 @@
+module Sites.Streams where
+
+-- TODO: refactor the site into
+-- * blogs
+-- * reading
+-- * projects
+-- * others?
+-- TODO: rename Erochest to Home
+-- TODO: Home aggregates all streams into Atom and the index page
+-- TODO: each stream has its own pages, templates, and feed
