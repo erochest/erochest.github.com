00244410dd02aef6c428f18b9da970ac1fe739ca
Eric Rochester <erochest@virginia.edu>
2016-10-21 06:56:17 -0400

    errsite draft

--- app/Actions.hs
+++ app/Actions.hs
@@ -7,11 +7,13 @@ module Actions where
 import           Types
 
 import           Sites.Actions.Hakyll
+import           Sites.Actions.Draft
 import           Sites.Actions.Deploy
 import           Sites.Actions.Illiterate
 
 
 action :: Actions -> IO ()
 action (Hakyll args) = callHakyll args
+action Draft{..}     = newDraft draftCategory draftTags draftTitle draftSlug
 action Deploy{..}    = deploySite scratch bail
 action Illiterate    = illiterateClojure
---
+++ src/Sites/Actions/Draft.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+
+module Sites.Actions.Draft where
+
+
+import           Data.Char         (isAlphaNum)
+import qualified Data.HashSet      as S
+import qualified Data.List         as L
+import           Data.Maybe        (fromMaybe)
+import qualified Data.Text         as T
+import           Data.Text.Format
+import qualified Data.Text.Lazy    as TL
+import qualified Data.Text.Lazy.IO as TLIO
+import           Data.Time
+import           Shelly            hiding (FilePath, path, (<.>), (</>))
+import           System.Directory
+import           System.FilePath
+
+
+newDraft :: FilePath -> S.HashSet T.Text -> T.Text -> Maybe FilePath -> IO ()
+newDraft cat tagSet title mslug = shelly $ verbosely $ do
+    now <-  liftIO
+        $   formatTime defaultTimeLocale (iso8601DateFormat (Just "%H:%M:%SZ"))
+        <$> getCurrentTime
+    git_ "checkout" ["-b", T.pack slug]
+    liftIO $ createDirectoryIfMissing True cdir
+    liftIO
+        $ TLIO.writeFile path
+        $ format "---\ntitle: {}\ndate: {}\ncategories: {}\n---\n\n<!--more-->\n"
+        ( title, now, tags)
+    git_ "add" [T.pack path]
+    git_ "commit" ["-m", TL.toStrict $ format "Stub for '{}'." $ Only title]
+    where
+        slug = fromMaybe (T.unpack $ slugify title) mslug
+        cdir = "posts" </> cat
+        path = cdir </> slug <.> "md"
+        tags = T.intercalate " " $ L.sort $ S.toList tagSet
+
+slugify :: T.Text -> T.Text
+slugify = T.map slugChar . T.toLower
+    where
+        slugChar x = if isAlphaNum x
+                        then x
+                        else '-'
+
+git_ :: T.Text -> [T.Text] -> Sh ()
+git_ = command1_ "git" []
