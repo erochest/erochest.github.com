84c991b3c9e3e89b82665cb6105f153fb788a092
Eric Rochester <eric.rochester@willowtreeapps.com>
2017-04-06 06:58:56 -0400

    Updated test.

--- specs/Sites/Actions/DraftSpec.hs
+++ specs/Sites/Actions/DraftSpec.hs
@@ -1,23 +1,33 @@
+{-# LANGUAGE OverloadedLists   #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE OverloadedLists #-}
 
 
 module Sites.Actions.DraftSpec where
 
 
+import           Data.Monoid
+import qualified Data.Text           as T
+import           Data.Time
+import           Shelly              (shelly)
 import           Test.Hspec
 
-import           Sites.Actions.Draft (yamlHeader)
+import           Sites.Actions.Draft (renderHeader, yamlHeader)
 
 
 spec :: Spec
 spec = describe "yamlHeader" $ do
-    it "should escape titles with colons." $
-        yamlHeader "title: hi" ["tag"] "timestamp" `shouldBe` "\
-            \---\n\
-            \categories:\n\
-            \- tag\n\
-            \date: timestamp\n\
-            \title: ! 'title: hi'\n\
-            \---\n\
-            \"
+    it "should escape titles with colons." $ do
+      now <- formatTime defaultTimeLocale
+             (iso8601DateFormat (Just "%H:%M:%SZ"))
+             <$> getCurrentTime
+      header <- shelly $ yamlHeader "title: hi" ["tag"]
+
+      renderHeader header `shouldBe`
+        "\
+        \---\n\
+        \categories:\n\
+        \- tag\n\
+        \date: " <> T.pack now <> "\n\
+        \title: ! 'title: hi'\n\
+        \---\n\
+        \\n"
