6fec2730eae70310fd51167e01815e5d56e99157
Eric Rochester <erochest@gmail.com>
2024-09-21 15:01:43 -0400

    parallel io with mmap

---
+++ content/posts/parallel-io-with-mmap/project.clj
@@ -0,0 +1,9 @@
+(defproject parallel-io-with-mmap "0.1.0-SNAPSHOT"
+  :description "Parallel IO with mmap"
+  :url "http://www.ericrochester.com/pages/code/parallel-io-with-mmap/"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [nio "0.0.5"]
+                 [org.apache.commons/commons-lang3 "3.1"]
+                 [clj-time "0.5.0"]]
+  :source-paths ["."])
+
